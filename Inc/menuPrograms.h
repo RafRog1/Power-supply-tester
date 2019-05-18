df0386e2ff40d43134ae217e28287c39c8aad6a3
Rafał Rogalski <rogalski.ra@gmail.com>
2019-05-18 14:11:27 +0200

    menuPrograms files added

---
+++ Inc/menuPrograms.h
@@ -0,0 +1,13 @@
+#ifndef __menu_programs_H
+#define __menu_programs_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__menu_programs_H */
+
---
+++ Src/menuPrograms.c
@@ -0,0 +1,2 @@
+#include "menuPrograms.h"
+
