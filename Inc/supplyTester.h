60931b23a8182fda4f600f1bd786df470cfe6ab4
Rafał Rogalski <rogalski.ra@gmail.com>
2019-05-29 00:37:05 +0200

    Added supplyTester code

--- Inc/supplyTester.h
+++ Inc/supplyTester.h
@@ -4,7 +4,17 @@
  extern "C" {
 #endif
 
+#include <stdint.h>
 
+#define DEFAULT_CORRECTION_RESISTANCE 1.0
+
+struct supplyTester_Struct{
+	float correctionResistance;
+};
+
+void supplyTesterInitStruct(void);
+void increaseCorrectionResistance(void);
+char* getCorrectionResistanceString(void);
 
 #ifdef __cplusplus
 }
--- Src/supplyTester.c
+++ Src/supplyTester.c
@@ -1,1 +1,37 @@
 #include "supplyTester.h"
+#include "system.h"
+#include "menu.h"
+#include <stdio.h>
+
+struct supplyTester_Struct supplyTester;
+
+void supplyTesterInitStruct(void){
+	supplyTester.correctionResistance = DEFAULT_CORRECTION_RESISTANCE;
+}
+
+void increaseCorrectionResistance(void){
+	float increase;
+
+	if(fastButtonIsEnable())
+		increase = 0.5;
+	else
+		increase = 0.1;
+
+	if(supplyTester.correctionResistance == 3.0){
+		supplyTester.correctionResistance = 0.1;
+		disableFastButton();
+		return;
+	}
+	supplyTester.correctionResistance += increase;
+	if(supplyTester.correctionResistance > 3.0){
+		supplyTester.correctionResistance = 3.0;
+		disableFastButton();
+	}
+}
+char* getCorrectionResistanceString(void){
+	static char correctionResistanceString[5];
+	uint8_t num1 = supplyTester.correctionResistance;
+	uint8_t num2 = (supplyTester.correctionResistance - num1) * 10;
+	sprintf(correctionResistanceString, "%d.%d", num1, num2);
+	return correctionResistanceString;
+}
