986a123f550c8f79fe9417ee3067e0cd00b2ca70
Rafał Rogalski <rogalski.ra@gmail.com>
2019-06-11 23:53:29 +0200

    Added enum witch actual on relay and resistance

--- Inc/supplyTester.h
+++ Inc/supplyTester.h
@@ -8,6 +8,19 @@
 
 #define DEFAULT_CORRECTION_RESISTANCE 1.0
 
+#define RELAY_1 80.0
+#define RELAY_1_2 26.7
+#define RELAY_1_2_3 11.4
+#define RELAY_1_2_3_4 5.3
+
+enum actualRelayWork{
+	none,
+	relay1, //80ohm
+	relay1and2, //26,7ohm
+	relay1and2and3, //11,4ohm
+	relay1and2and3and4, //5,3ohm
+};
+
 struct supplyTester_Struct{
 	float correctionResistance;
 };
